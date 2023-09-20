b2a3f1edfdb582b182f56c0bcacea69e12259ad1
janitha thalagoda <112193392+janitha1205@users.noreply.github.com>
2023-09-20 16:12:50 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-
-import Component1 from "./Component/component1";
-import Titel1 from "./page/title";
-import Home1 from "./page/home";
-import About1 from "./page/about";
-import Gallery1 from "./page/gallery";
-import Dynamiccomp1 from "./page/dynamiccomp";
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import BarChart from './Component/BarChart';
-
-import { useState } from "react";
-//import Gallery from "./page/gallery";
-
+import logo from './logo.svg';
+import './App.css';
+import Counter from './component/counter';
 function App() {
-  
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+       <Counter/>
+       
+      </header>
+    </div>
+  );
+}
 
-//<BarChart/>
-
-
-
-    return (
-     // <div className="App">
-
-<BrowserRouter>
-<Routes>
-<Route path='/' element={<Home1/>}></Route>
-<Route path='/about' element={<About1/>}></Route>
-<Route path='/gallery' element={<Gallery1/>}></Route>
-
-
-</Routes>
-
-
-</BrowserRouter>
-
-
-    //  </div>
-
-    );
-  }
-  
-  export default App;
+export default App;
